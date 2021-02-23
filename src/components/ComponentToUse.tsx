39c9e9a584bfe46293cc472332a131ca38ae4d89
William Mascarello <william.mascarello@fcid.com.br>
2021-02-23 17:44:08 -0300

    Base of Project

---
+++ src/components/ComponentToUse.tsx
@@ -0,0 +1,9 @@
+// Componens are reaproveitables in any parts of the App
+
+export function ComponentToUse() {
+    return (
+        <header>
+            <h2>Component to Use in the App</h2>
+        </header>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { ComponentToUse } from "../components/ComponentToUse";
+// This is the main and start page of the App
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Move It</h1>
+      <ComponentToUse />
+    </div>
+  )
+}
